3b397b650bb7ef1c02f0af94982f00c5eac44707
Luke Young <me@innoying.com>
2012-01-05 15:53:49 -0600

    Edited OI to have basic framework for joysticks.

--- src/org/inceptus/OI.java
+++ src/org/inceptus/OI.java
@@ -5,11 +5,18 @@
 
 package org.inceptus;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 public class OI {
     
     private static OI instance = null;
     
+    private Joystick leftStick;
+    private Joystick rightStick;
+    
     private OI() {
+        leftStick = new Joystick();
+        rightStick = new Joystick();
     }
     
     public static OI getInstance() {
