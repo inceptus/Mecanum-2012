7b7949981bac6ac2e04037a36ee03d695dee79c7
Luke Young <me@innoying.com>
2012-01-05 15:56:26 -0600

    Added org.inceptus.constants

---
+++ src/org/inceptus/Constants.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.inceptus;
+
+/**
+ *
+ * @author innoying
+ */
+public class Constants {
+    //Motors
+    public static final int kFrontLeftMotor = 1;
+    public static final int kFrontRightMotor = 2;
+    public static final int kRearLeftMotor = 4;
+    public static final int kRearRightMotor = 3;
+    //Joysticks
+    public static final int kLeftJoystick = 1;
+    public static final int kRightJoystick = 2;
+}
